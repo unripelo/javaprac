5fd7d9f7ad049bad61dc1f3d44f2e9babad27d37
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-02-15 23:45:54 +0800

    adding abc

---
+++ abc.java
@@ -0,0 +1,9 @@
+public class abc {
+    public static void main(String[] args) {
+        System.out.println("\nCharacters A-Z:");
+        for (char c = 'A'; c <='Z'; c++) {
+            System.err.println(c + "");
+        }
+        System.out.println();
+    }
+}
